3296d575cd6ba6615633a8d5219d6ae0f6e3e373
val5789 <valematias1@gmail.com>
2024-11-20 13:28:51 -0500

    cambio 15

--- pokedex-backend-main/src/rutas/rutasUsuarios.js
+++ pokedex-backend-main/src/rutas/rutasUsuarios.js
@@ -2,10 +2,6 @@ const express = require('express');
 const enrutador = express.Router();
 const usuarioControlador = require('../controladores/usuarioControlador');
 
-// Ruta para registrar un usuario
 enrutador.post('/registrar', usuarioControlador.registrarUsuario);
-
-// Ruta para listar usuarios
-enrutador.get('/listar', usuarioControlador.listarUsuarios);
 
 module.exports = enrutador;
