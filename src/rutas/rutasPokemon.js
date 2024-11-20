7c06a2ab25f41e22b40e57f2f39b78cdb9872645
val5789 <valematias1@gmail.com>
2024-11-20 11:56:04 -0500

    cambio 7

--- src/rutas/rutasPokemon.js
+++ src/rutas/rutasPokemon.js
@@ -3,5 +3,7 @@ const enrutador = express.Router();
 const pokemonControlador = require('../controladores/pokemonControlador');
 
 enrutador.post('/registrar', pokemonControlador.registrarPokemon);
+// Ruta para listar Pokémon
+enrutador.get('/listar', pokemonControlador.listarPokemones);
 
 module.exports = enrutador;
--- src/rutas/rutasUsuario.js
+++ src/rutas/rutasUsuario.js
@@ -3,5 +3,7 @@ const enrutador = express.Router();
 const usuarioControlador = require('../controladores/usuarioControlador');
 
 enrutador.post('/registrar', usuarioControlador.registrarUsuario);
+// Ruta para listar Pokémon
+enrutador.get('/listar', pokemonControlador.listarPokemones);
 
 module.exports = enrutador;
